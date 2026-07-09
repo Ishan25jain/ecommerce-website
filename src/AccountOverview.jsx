b415fbb509a51ac31a9b2af1c2b5f4278fa03866
Ishan25jain <jishan13146@gmail.com>
2026-07-09 19:58:48 +0530

    describe your change here

--- src/AccountOverview.jsx
+++ src/AccountOverview.jsx
@@ -3,9 +3,6 @@ import { Link } from 'react-router-dom'
 
 function AccountOverview() {
   const [user, setUser] = useState({
-    firstName: 'Ishan',
-    lastName: 'Jain',
-    email: 'ishanjain@gmail.com'
   })
 
   useEffect(() => {
@@ -25,10 +22,10 @@ function AccountOverview() {
       <section className="account-section">
         <h2 className="account-section-title">Personal Details</h2>
         <p className="account-details-text">
-          {user.firstName} {user.lastName} , {user.email}
+          {user.firstName} {user.lastName}  {user.email}
         </p>
         <div className="account-links">
-          <Link to="/account/personal-details">View Personal Details</Link>
+          <Link to="/account/personal-details">Edit Personal Details</Link>
           <Link to="/account/personal-details">Change Your Password</Link>
         </div>
       </section>
